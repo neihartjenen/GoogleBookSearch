cfe3fce65b034898a1d98aed3b844d744e4b4ae3
steven Reinart <stevenreinart123@gmail.com>
2020-06-22 21:33:42 -0700

    start

---
+++ create-react-express/client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./App.css";
+import NavBar from "./components/navbar";
+import Jumbotron from "./components/jumbotron";
+
+class App extends Component {
+  render() {
+    return (
+      <NavBar/>
+      <Jumbotron></Jumbotron>
+      // navbar
+      // jumbotron
+      // form 1 input
+      // results
+      
+    );
+  }
+}
+
+export default App;
---
+++ create-react-express/client/src/components/jumbotron/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Jumbotron() {
+   return ( 
+    <div class="jumbotron">
+    <h1 class="display-4">(React) Google Books Search</h1>
+    <p class="lead">Search far and Save Books of Interest</p>
+    <hr class="my-4"></hr>
+    </div>
+        
+    )
+}
